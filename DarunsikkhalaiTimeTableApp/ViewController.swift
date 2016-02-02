ea401d012aa6c799f328e6fd84400dd4c520c53c
Hadespipe <hadespipe@gmail.com>
2016-02-02 20:38:17 +0700

    Initial Commit

---
+++ DarunsikkhalaiTimeTableApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DarunsikkhalaiTimeTableApp
+//
+//  Created by jarukit boonkerd on 2/2/2559 BE.
+//  Copyright © 2559 Hades corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
